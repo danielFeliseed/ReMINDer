ce001425b40f61db51e8e3ceee481f55615dfaf4
danielFeliseed <daniel@feliseed.co.jp>
2023-12-18 18:49:04 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <button><nuxt-link to="/">Go to home</nuxt-link></button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1 class=" text-2xl font-bold text-purple-900">Home</h1>
+        <div class="flex flex-col">
+            <div>
+                <p>Heyyyy</p>
+            </div>
+            <p>heyyyy</p>
+        </div>
+        <button><nuxt-link to="/about">Go to about</nuxt-link></button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
