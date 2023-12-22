1551082fd2b60199aaeea34e2187491cc675db15
danielFeliseed <daniel@feliseed.co.jp>
2023-12-22 12:23:57 +0900

    Changed function to arrow functions toggleModal

--- pages/support.vue
+++ pages/support.vue
@@ -16,7 +16,7 @@ function toggleModal() {
             
 
 <!-- Modal toggle -->
-<button @click="toggleModal" data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+<button @click="() => ariaHidden = !ariaHidden" data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
   Read our FAQs
 </button>
 
@@ -68,7 +68,7 @@ function toggleModal() {
             </div>
             <!-- Modal footer -->
             <div class="flex justify-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <button @click="toggleModal" data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Close</button>
+                <button @click="() => ariaHidden = !ariaHidden" data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Close</button>
                 
             </div>
         </div>
