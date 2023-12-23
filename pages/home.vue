5c8c8c2c9f772956fc829f7152c6ba4c1909d8ad
danielFeliseed <daniel@feliseed.co.jp>
2023-12-23 23:02:51 +0900

    Finished the first rough draft of the complete app

--- pages/home.vue
+++ pages/home.vue
@@ -7,6 +7,7 @@ const supabase = useSupabaseClient()
 const user  = useSupabaseUser()
 const newDiary = ref('')
 const diaries = ref([])
+let toastHidden = ref(true)
 
 console.log(user.value)
 async function getDiaries() {
@@ -27,6 +28,7 @@ async function getDiaries() {
     } else {
       diaries.value.push(data)
       newDiary.value = ''
+      toastHidden.value = !toastHidden.value
       getDiaries()
     }
   }
@@ -39,7 +41,22 @@ async function getDiaries() {
 <template>
     
 
-<div class="flex flex-col items-center justify-center space-y-4 mt-[300px]">
+<div  class="flex flex-col items-center justify-center space-y-4 mt-[300px]">
+  <div v-show="!toastHidden" id="toast-success" class="mt-[60px] fixed flex items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow top-5 right-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
+    <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+        </svg>
+        <span class="sr-only">Check icon</span>
+    </div>
+    <div class="ms-3 text-sm font-normal">Item moved successfully.</div>
+    <button @click="() => toastHidden = !toastHidden" type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
+        <span class="sr-only">Close</span>
+        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+        </svg>
+    </button>
+</div>
     
 <label for="message" class="block mb-2 text-xl font-medium text-gray-900 dark:text-white">Your Diary</label>
 <textarea id="message" rows="4" class="block p-2.5 w-2/3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
