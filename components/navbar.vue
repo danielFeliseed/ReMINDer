832e41dde4a485c7b7595a8108d44751f5547e11
danielFeliseed <daniel@feliseed.co.jp>
2024-01-21 19:15:47 +0900

    added shadow on navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -16,7 +16,7 @@ console.log(user.value)
 
 <template>
 
-<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 shadow-md">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a v-if="user" href="/home" class="flex items-center space-x-3 rtl:space-x-reverse">
         <span class="header-text self-center text-2xl font-semibold whitespace-nowrap ">ReMINDer</span>
