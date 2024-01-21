08897369bb6d002177db226659be92e489c6b1fe
danielFeliseed <daniel@feliseed.co.jp>
2024-01-21 20:08:11 +0900

    Fixed button sizes on login and sign up

--- pages/index.vue
+++ pages/index.vue
@@ -62,11 +62,11 @@ async function signOut() {
 
 
 <template>
-  <div class="app-container flex flex-col items-center justify-center space-y-4">
+  <div class="app-container flex flex-col items-center justify-center space-y-4 mt-[200px]">
 
 <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
     <form class="space-y-6" action="#">
-        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
+        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Log in</h5>
         <div>
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
             <input v-model="email" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required>
@@ -86,7 +86,7 @@ async function signOut() {
         </div>
         <nuxt-link href="#" @click="signIn">
         <button type="submit"
-          class="button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-3">Submit</button>
+          class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-3">Submit</button>
       </nuxt-link>
         <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
             Not registered? <a href="/signUp" class="text-blue-700 font-bold hover:underline dark:text-blue-500">Create account</a>
@@ -113,7 +113,6 @@ async function signOut() {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
   background: linear-gradient(120deg, #ffffff 0%, #feffff 100%);
 }
 
