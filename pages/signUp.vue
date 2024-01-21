335ac4b59a1772359105c4f7a6b50068d746baec
danielFeliseed <daniel@feliseed.co.jp>
2024-01-22 01:23:41 +0900

    g

--- pages/signUp.vue
+++ pages/signUp.vue
@@ -50,8 +50,8 @@ function signOut() {
   <div class="flex flex-col items-center justify-center space-y-4 mt-[200px]">
 
     <transition name="fade">
-    <div v-show="!toastHidden" id="toast-danger" position="sticky"
-      class="fixed mt-[100px] flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+    <div v-show="toastHidden" id="toast-danger" position="sticky"
+      class=" flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
       role="alert">
       <div
         class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
